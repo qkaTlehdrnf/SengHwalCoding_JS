65be9e274e77fb1d082a9cc501b2b683f13f8e38
Kim,Won-Joong <wonjoong11@naver.com>
2022-01-25 22:13:43 +0900

    0125

---
+++ main.js
@@ -0,0 +1,7 @@
+var text = 'Hello World yo man';
+console.log(text);
+var memberArray = ['egoing', 'graphittie', 'leezhce'];
+console.log(memberArray[2]);
+var memberObject = {
+    manager: 'egoing'
+};
---
+++ main.ts
@@ -0,0 +1,9 @@
+const text: string = 'Hello World yo man';
+console.log(text);
+
+var memberArray = ['egoing', 'graphittie', 'leezhce']
+console.log(memberArray[2]);
+
+var memberObject = {
+    manager: 'egoing'
+}
